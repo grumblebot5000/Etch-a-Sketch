59d3021c61d0baae1c11d280eb634afc9519776e
Grumble <semhof@protonmail.com>
2020-09-21 16:42:54 -0400

    Draw lines with mouse pointer

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,6 +4,12 @@ let paperResolution; // how many columns (and rows) user wants
 // WHEN PAGE IS LOADED, GET A GRID SIZE
 window.addEventListener('load', initPage);
 
+// When mouse enters gridBox, change the background color
+document.addEventListener('mousemove', function(e){
+	let idNum = e.target.id;
+	drawLine(idNum);
+})
+
 // RESET THE PAGE and GET RESOLUTION FOR NEXT PAGE //
 function initPage(){
 	paperResolution = prompt('How many columns/rows would you like?');
@@ -14,9 +20,6 @@ function initPage(){
 function drawGrid(num){
 	// 960/(num of columns = gridBox width & height)
 	let gridHW = 960/num;
-	console.log('gridHW: ' + gridHW);
-	console.log('paperResolution: ' + paperResolution);
-	console.log('num: ' + num);
 
 	//for each row, create a gridLineContainer
 	for(let i=1; i<=paperResolution; i++){
@@ -28,16 +31,17 @@ function drawGrid(num){
 		for(let x=1; x<=num; x++){
 			let gridBox = document.createElement('div')
 			gridBox.className = 'gridBoxInit';
-			gridBox.id = x;
-			gridBox.textContent = x-x;
-			gridBox.style.color = 'white';
-			gridBox.height = gridHW + 'px';
+			gridBox.textContent = '.';
 			gridBox.style.lineHeight = gridHW + 'px';
-			console.log('grid Height = ' + gridHW + 'px');
 			gridLineContainer.appendChild(gridBox);
+			gridBox.id = i + '-' + x;
 		}
-
+		
 		// append row to gridContainer div
 		document.getElementById('gridContainer').appendChild(gridLineContainer);
 	}
+}
+
+function drawLine(idNum){
+	document.getElementById(idNum).className = 'gridBoxDrawn';
 }
